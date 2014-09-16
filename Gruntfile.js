3e327fc78a441b04e2b10de72bbd9e5556662a6d
Brian Birtles <birtles@gmail.com>
2014-09-16 11:06:59 +0900

    Initial commit of travis setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,47 @@
 module.exports = function(grunt) {
   var port = grunt.option('port') || 8000;
 
-  grunt.initConfig({
-    shell: {
-      build: {
-        options: {
-          stdout: true
-        },
-        command: 'bikeshed.py spec Overview.src.html'
-      },
-      update: {
-        options: {
-          stdout: true
-        },
-        command: 'bikeshed.py update'
-      }
-    },
+  grunt.initConfig({});
 
-    watch: {
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.config('shell', {
+    build: {
       options: {
-        livereload: true,
+        stdout: true
       },
-      files: [ 'Overview.src.html', 'biblio.json' ],
-      tasks: 'default'
+      command: 'bikeshed spec Overview.src.html'
     },
+    update: {
+      options: {
+        stdout: true
+      },
+      command: 'bikeshed update'
+    }
+  });
 
-    express: {
-      all: {
-        options: {
-          bases: [ '.' ],
-          port: port,
-          hostname: 'localhost',
-          livereload: true
-        }
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.config('watch', {
+    options: {
+      livereload: true,
+    },
+    files: [ 'Overview.src.html', 'biblio.json' ],
+    tasks: 'default'
+  });
+
+  grunt.loadNpmTasks('grunt-express');
+  grunt.config('express', {
+    all: {
+      options: {
+        bases: [ '.' ],
+        port: port,
+        hostname: 'localhost',
+        livereload: true
       }
-    },
+    }
+  });
 
+  grunt.loadNpmTasks('grunt-open');
+  grunt.config('open', {
     open: {
       all: {
         path: 'http://localhost:' + port + '/Overview.html'
@@ -43,15 +49,47 @@ module.exports = function(grunt) {
     },
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-express');
-  grunt.loadNpmTasks('grunt-open');
-  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.config('copy', {
+    spec: {
+      nonull: true, /* error if we haven't build the spec yet */
+      src: [ 'Overview.html', '*.css', 'img/*', 'MathJax/*' ] ,
+      dest: 'publish/',
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-gh-pages');
+  grunt.config('gh-pages', {
+    spec: {
+      options: {
+        base: 'publish',
+        message: process.env.COMMIT_MESSAGE || 'Auto-update of spec'
+      },
+      src: ['**/*']
+    }
+  });
+
+  // Use token if available and push to https URL
+  if (process.env.GH_TOKEN && process.env.TRAVIS_REPO_SLUG) {
+    grunt.config.merge({
+      'gh-pages': {
+        spec: {
+          options: {
+            repo: 'https://' + process.env.GH_TOKEN + '@github.com/'
+                  + process.env.TRAVIS_REPO_SLUG + '.git',
+            silent: true
+          }
+        }
+      }
+    });
+  }
 
   grunt.registerTask('default', ['shell:build']);
-  grunt.registerTask('edit', [ 'shell:update',
-                               'shell:build',
-                               'express',
-                               'open',
-                               'watch']);
+  grunt.registerTask('build', ['shell:build']);
+  grunt.registerTask('publish', ['copy:spec']);
+  grunt.registerTask('upload', ['gh-pages:spec']);
+  grunt.registerTask('live-edit', [ 'shell:build',
+                                    'express',
+                                    'open',
+                                    'watch']);
 };
