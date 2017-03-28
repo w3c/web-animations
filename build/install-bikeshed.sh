369f92c1323a0f79ad693e89ee8810121c16f72e
Brian Birtles <birtles@gmail.com>
2017-03-28 11:22:44 +0900

    Try converting to a python project

--- build/install-bikeshed.sh
+++ build/install-bikeshed.sh
@@ -7,9 +7,7 @@ function error_exit
 }
 
 echo "Installing bikeshed dependencies..."
-sudo apt-get update
 sudo apt-get install python2.7 python-dev libxslt1-dev libxml2-dev || error_exit "Error installing Python and dependencies"
-python --version
 sudo pip install lxml || error_exit "Error installing pip libxml"
 sudo pip install lxml --upgrade || error_exit "Error upgrading libxml"
 sudo pip install pygments || error_exit "Error installing Pygments"
